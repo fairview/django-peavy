2469a1bfb4e10749fd53e56a83eea07b3e81e52f
John Hensley <john@fairviewcomputing.com>
2011-09-28 10:48:49 -0400

    Bump version number to 0.5.1.
    For timestamp index, origin_server default change.

--- peavy/__init__.py
+++ peavy/__init__.py
@@ -1,7 +1,7 @@
 """
 django-peavy makes it easy to collect and monitor Django application logging.
 """
-VERSION = (0, 5, 0)
+VERSION = (0, 5, 1)
 
 def get_version():
     return '.'.join((str(d) for d in VERSION))
